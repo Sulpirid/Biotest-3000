00b964e1817a9998220e64a1b1a670200e327894
lukmad <lukas.madej42@gmail.com>
2025-12-01 14:14:24 +0100

    Initial Commit

---
+++ test.program/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  test.program
+//
+//  Created by Lukáš Madej on 01/12/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ test.program/test_programApp.swift
@@ -0,0 +1,17 @@
+//
+//  test_programApp.swift
+//  test.program
+//
+//  Created by Lukáš Madej on 01/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct test_programApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
